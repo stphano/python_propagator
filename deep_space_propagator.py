5e34aa28c00019582a349e4a11b489b12ceed57f
stphano <58521117+stphano@users.noreply.github.com>
2021-02-12 16:57:54 +0100

    Update deep_space_propagator.py

--- deep_space_propagator.py
+++ deep_space_propagator.py
@@ -6,9 +6,30 @@ from tudatpy.kernel.simulation import propagation_setup
 from tudatpy.kernel.astro import conversion
 from tudatpy.io import save2txt
 from matplotlib import pyplot as plt
+
+# Problem-specific imports
+from defining_thrust import defining_thrust
+from defining_thrust import get_thrust_acceleration_model_from_parameters
+import defining_thrust as Util
+
 # # define initial and end epoch
 simulation_start_epoch = 10000.0
-simulation_end_epoch = 300*constants.JULIAN_DAY
+simulation_end_epoch = constants.JULIAN_DAY
+
+# define thrust parameters
+thrust_parameters = [0.5,
+                     24 / 5,
+                     -0.03344538412056863,
+                     -0.06456210720352829,
+                     0.3943447499535977,
+                     0.5358478897251189,
+                     -0.8607350478880107,
+                     -0.03344538412056863,
+                     -0.06456210720352829,
+                     0.3943447499535977,
+                     0.5358478897251189,
+                     -0.8607350478880107,
+                     ]
 # # load spice ephemerides
 spice_interface.load_standard_kernels()
 # # define settings for the celestial bodies and propagation
@@ -21,23 +42,28 @@ body_settings = environment_setup.get_default_body_settings(
 bodies = environment_setup.create_system_of_bodies(body_settings)
 
 # #  Create veichle
-bodies.create_empty_body("3U-CubeSat")
+bodies.create_empty_body("3UCubeSat")
 # set mass
-bodies.get_body("3U-CubeSat").set_constant_mass(4.0)
+bodies.get_body("3UCubeSat").set_constant_mass(4.0)
 # set radiation coefficients
 reference_area_radiation = 0.4
 radiation_pressure_coefficient = 1.2
+constant_specific_impulse = 311.0  # s
 occulting_bodies = []
 radiation_pressure_settings = environment_setup.radiation_pressure.cannonball(
     "Sun", reference_area_radiation, radiation_pressure_coefficient, occulting_bodies
 )
 environment_setup.add_radiation_pressure_interface(
-    bodies, "3U-CubeSat", radiation_pressure_settings)
+    bodies, "3UCubeSat", radiation_pressure_settings)
 
 # # create accelerations
-bodies_to_propagate = ["3U-CubeSat"]
+bodies_to_propagate = ["3UCubeSat"]
 central_bodies = ["Sun"]
 accelerations_settings_3u_cubesat = dict(
+    bodies_to_propagate = [get_thrust_acceleration_model_from_parameters(thrust_parameters,
+                                                              bodies,
+                                                              simulation_start_epoch,
+                                                              constant_specific_impulse)],
    # "3U-CubeSat" :
    # [
    #     propagation_setup.acceleration.thrust_acceleration()
@@ -89,7 +115,7 @@ accelerations_settings_3u_cubesat = dict(
     ]
 )
 
-acceleration_settings = {"3U-CubeSat": accelerations_settings_3u_cubesat}
+acceleration_settings = {"3UCubeSat": accelerations_settings_3u_cubesat}
 acceleration_models = propagation_setup.create_acceleration_models(
     bodies,
     acceleration_settings,
@@ -110,8 +136,8 @@ initial_state = conversion.keplerian_to_cartesian(
 )
 # define the variables we need
 dependent_variables_to_save = [
-    propagation_setup.dependent_variable.total_acceleration("3U-CubeSat"),
-    propagation_setup.dependent_variable.keplerian_state("3U-CubeSat", "Sun"),
+    propagation_setup.dependent_variable.total_acceleration("3UCubeSat"),
+    propagation_setup.dependent_variable.keplerian_state("3UCubeSat", "Sun"),
 ]
 # create propagation settings
 propagator_settings = propagation_setup.propagator.translational(
@@ -137,6 +163,6 @@ states = dynamics_simulator.state_history
 dependent_variables = dynamics_simulator.dependent_variable_history
 
 save2txt(solution=states,
-         filename="PropagationHistory_3U_CubeSat_2.dat",
+         filename="PropagationHistory_3U_CubeSat_thrust.dat",
          directory="./",  # default = "./"  # default = None
          )
